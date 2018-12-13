03a2360b1e49cdfad1790ba16d1af532b0286d90
Carden Z <ephemere@protonmail.com>
2018-12-13 03:44:12 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,60 @@
+require 'pry'
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [6,4,2],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]
+]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, position, char)
+  board[position] = char
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def position_exzist(index)
+   index.between?(0, 8)
+end
+
+def valid_move?(board, index)
+  position_exzist(index) && !position_taken?(board, index)
+end
+
+def turn(board)
+	puts "Please enter 1-9:"
+	user_input = gets.chomp.strip
+	index = input_to_index(user_input)
+	if !valid_move?(board,index)
+		puts "invalid"
+		user_input = gets.chomp.strip
+	else
+    board[index] = "X"
+	end
+	display_board(board)
+end
+
+def turn_count(board)
+  board.select {|space| space != " " && space != nil}.length
+end
+
+def current_player(board)
+	turn_count(board).even? ? "X" : "O"
+end
